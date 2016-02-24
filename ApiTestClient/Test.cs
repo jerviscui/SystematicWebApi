75a84842f2be95164f84c0c601e11824ced2ec75
Jervis <cuijervis@126.com>
2016-02-24 18:26:48 +0800

    add HttpClient

---
+++ ApiTestClient/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestClient
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            
+        }
+    }
+}
